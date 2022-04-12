93690c49da7e110d261c3f03138d4c904e7fd2c5
Benni Simarmata <bennisetiady111@gmail.com>
2022-04-12 15:43:45 +0700

    fix subhome media query

--- src/components/SubHome.js
+++ src/components/SubHome.js
@@ -22,20 +22,6 @@ export function SubHome() {
         <div className='subHome'>
             <h1>“Let's make something<br/>out of nothing”</h1>
             <button><a href='#About'>Get Started</a></button>
-            <div className='socialMedia'>
-                <div className='Linked' onClick={Linked1}>
-                    <img src={Linked}></img>
-                    <a>benni-setiady</a>
-                </div>
-                <div className='Github' onClick={Github1}>
-                    <img src={Github}></img>
-                    <a>Bennismrt</a>
-                </div>
-                <div className='Insta' onClick={Insta1}>
-                    <img src={Insta}></img>
-                    <a>Bennysmrt</a>
-                </div>
-            </div>
         </div>
     )
 }
